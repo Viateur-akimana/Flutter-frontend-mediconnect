b58c13456519056658d0e0b92295f796139eb730
viateur <akimanaviateur94@gmail.com>
2024-05-21 07:45:01 +0200

    home screen implementation

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,34 @@
+// lib/screens/home_screen.dart
+import 'package:flutter/material.dart';
+import '../widgets/header.dart';
+import '../widgets/footer.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: Header(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to MediConnect',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Summon Emergency Help'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Schedule Appointments'),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Footer(),
+    );
+  }
+}
