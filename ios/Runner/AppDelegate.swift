77be3458bbf75e806bbcf4668431f4beb79ee5f4
viateur <akimanaviateur94@gmail.com>
2024-06-02 21:53:18 +0200

    Google map key integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+     [GMSServices provideAPIKey:@"AIzaSyBkAzB6UX78XjJ1RWx30Qwn3Mvf9Vdd64Q"];
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
