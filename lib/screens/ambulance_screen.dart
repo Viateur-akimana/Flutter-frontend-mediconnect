db8108a3ac86307f26a6b97b6b09b333e4945746
viateur <akimanaviateur94@gmail.com>
2024-06-03 09:54:08 +0200

    Adding location package for tracking current user device location and asking perminssions

--- lib/screens/ambulance_screen.dart
+++ lib/screens/ambulance_screen.dart
@@ -1,9 +1,8 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:location/location.dart';
+import 'package:geocoding/geocoding.dart' as geocoding;
 import '../widgets/header.dart';
 import '../widgets/footer.dart';
 
@@ -11,58 +10,72 @@ class AmbulanceScreen extends StatefulWidget {
   const AmbulanceScreen({Key? key}) : super(key: key);
 
   @override
-  // ignore: library_private_types_in_public_api
   _AmbulanceScreenState createState() => _AmbulanceScreenState();
 }
 
 class _AmbulanceScreenState extends State<AmbulanceScreen> {
+  Location _locationController = Location();
   GoogleMapController? mapController;
-  late LatLng _ambulanceLocation;
-  late LatLng _currentLocation;
+  LatLng _ambulanceLocation = const LatLng(-1.9403, 29.8739);
+  LatLng _currentLocation = const LatLng(-1.65275, 29.50684);
   List<LatLng> polylineCoordinates = [];
   PolylinePoints polylinePoints = PolylinePoints();
-  late PolylineResult result;
-  Location location = Location();
+  static const LatLng _nyabihuLocation = LatLng(-1.65275, 29.50684);
+  static const LatLng _bugeseraLocation = LatLng(-2.23456, 30.14825);
+  LatLng? _currentP;
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    _getAmbulanceLocation();
     _getCurrentLocation();
     _getPolyline();
   }
 
-  Future<void> _getAmbulanceLocation() async {
-    // Set the ambulance location to Kigali, Rwanda coordinates
-    _ambulanceLocation = const LatLng(-1.9403, 29.8739);
-  }
-
   Future<void> _getCurrentLocation() async {
-    try {
-      LocationData currentLocation = await location.getLocation();
-      setState(() {
-        _currentLocation = LatLng(currentLocation.latitude!, currentLocation.longitude!);
-      });
-    } catch (e) {
-      print('Could not get the location: $e');
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+
+    _serviceEnabled = await _locationController.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await _locationController.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
     }
+
+    _permissionGranted = await _locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await _locationController.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final LocationData _locationData = await _locationController.getLocation();
+    setState(() {
+      _currentP = LatLng(_locationData.latitude!, _locationData.longitude!);
+    });
   }
 
   Future<void> _getPolyline() async {
     try {
-      result = await polylinePoints.getRouteBetweenCoordinates(
-        "",
-        PointLatLng(_currentLocation.latitude, _currentLocation.longitude),
-        PointLatLng(_ambulanceLocation.latitude, _ambulanceLocation.longitude),
-        travelMode: TravelMode.driving,
-      );
-      if (result.points.isNotEmpty) {
-        for (var point in result.points) {
-          polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      if (_currentP != null && _ambulanceLocation != null) {
+        PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+          '', // Replace with your Google Maps API key
+          PointLatLng(_currentP!.latitude, _currentP!.longitude),
+          PointLatLng(_ambulanceLocation.latitude, _ambulanceLocation.longitude),
+          travelMode: TravelMode.driving,
+        );
+        if (result.points.isNotEmpty) {
+          polylineCoordinates.clear();
+          for (var point in result.points) {
+            polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+          }
+          setState(() {});
+        } else {
+          print(result.errorMessage);
         }
-        setState(() {});
-      } else {
-        print(result.errorMessage);
       }
     } catch (e) {
       print('Error fetching polyline: $e');
@@ -73,34 +86,102 @@ class _AmbulanceScreenState extends State<AmbulanceScreen> {
     mapController = controller;
   }
 
-  @override
+  Future<void> _searchLocation(String query) async {
+    try {
+      List<geocoding.Location> locations = await geocoding.locationFromAddress(query);
+      if (locations.isNotEmpty) {
+        geocoding.Location location = locations.first;
+        LatLng searchedLocation = LatLng(location.latitude, location.longitude);
+        mapController?.animateCamera(
+          CameraUpdate.newCameraPosition(
+            CameraPosition(
+              target: searchedLocation,
+              zoom: 4.0,
+            ),
+          ),
+        );
+      } else {
+        print('No location found');
+      }
+    } catch (e) {
+      print('Error searching location: $e');
+    }
+  }
+
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Header(),
       body: Column(
         children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Search location',
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 15.0,
+                  ),
+                  border: InputBorder.none,
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search, color: Colors.grey[600]),
+                    onPressed: () {
+                      _searchLocation(_searchController.text);
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
           Expanded(
             child: GoogleMap(
               onMapCreated: _onMapCreated,
               initialCameraPosition: CameraPosition(
-                target: _currentLocation, // Set initial camera position to current location
-                zoom: 14.0,
+                target: _currentP ?? _currentLocation,
+                zoom: 8.0,
               ),
               markers: {
+                if (_currentP != null)
+                  Marker(
+                    markerId: MarkerId('currentLocation'),
+                    icon: BitmapDescriptor.defaultMarker,
+                    position: _currentP!,
+                    infoWindow: InfoWindow(
+                      title: 'Your Location',
+                      snippet: 'Your current location',
+                    ),
+                  ),
                 Marker(
                   markerId: MarkerId('ambulance'),
-                  position: _ambulanceLocation,
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _nyabihuLocation,
                   infoWindow: InfoWindow(
                     title: 'Ambulance Location',
                     snippet: 'Kigali, Rwanda',
                   ),
                 ),
                 Marker(
-                  markerId: MarkerId('currentLocation'),
-                  position: _currentLocation,
+                  markerId: MarkerId('bugeseraLocation'),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _bugeseraLocation,
                   infoWindow: InfoWindow(
-                    title: 'Your Location',
-                    snippet: 'Your current location',
+                    title: 'Bugesera Location',
+                    snippet: 'Bugesera, Rwanda',
                   ),
                 ),
               },
