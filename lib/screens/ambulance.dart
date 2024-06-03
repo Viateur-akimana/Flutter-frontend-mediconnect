fe0c364099ede2baaf8064ee262781e3bdf10096
viateur <akimanaviateur94@gmail.com>
2024-06-03 16:38:16 +0200

    geolocator package

--- lib/screens/ambulance.dart
+++ lib/screens/ambulance.dart
@@ -1,11 +1,11 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:location/location.dart';
-// import 'package:geocoding/geocoding.dart' as geocoding;
-import '../widgets/header.dart';  
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+import '../widgets/header.dart';
 import '../widgets/footer.dart';
 
 class AmbulanceScreen extends StatefulWidget {
@@ -17,27 +17,30 @@ class AmbulanceScreen extends StatefulWidget {
 
 class _AmbulanceScreenState extends State<AmbulanceScreen> {
   final _locationController = Location();
-    final Completer<GoogleMapController> _mapController =
+  final Completer<GoogleMapController> _mapController =
       Completer<GoogleMapController>();
   GoogleMapController? mapController;
- final LatLng _ambulanceLocation = const LatLng(-1.9403, 29.8739);
- final LatLng _currentLocation = const LatLng(-1.65275, 29.50684);
+  LatLng _ambulanceLocation = const LatLng(-1.9403, 29.8739);
+  final LatLng _currentLocation = const LatLng(-1.65275, 29.50684);
   List<LatLng> polylineCoordinates = [];
   PolylinePoints polylinePoints = PolylinePoints();
+  // ignore: unused_field
   static const LatLng _nyabihuLocation = LatLng(-1.65275, 29.50684);
   static const LatLng _bugeseraLocation = LatLng(-2.23456, 30.14825);
   LatLng? _currentP;
- final TextEditingController _searchController = TextEditingController();
- Map<PolylineId,Polyline> polylines = {};
+  final TextEditingController _searchController = TextEditingController();
+  Map<PolylineId, Polyline> polylines = {};
+  late IO.Socket socket;
 
   @override
   void initState() {
     super.initState();
+    initSocket();
     _getCurrentLocation().then((_) => {
-      getPolylinePoints().then((coordinates) =>{
-        generatePolyLineFromPoints(coordinates),
-      }),
-    });
+          getPolylinePoints().then((coordinates) => {
+                generatePolyLineFromPoints(coordinates),
+              }),
+        });
     getPolylinePoints();
   }
 
@@ -65,10 +68,12 @@ class _AmbulanceScreenState extends State<AmbulanceScreen> {
       setState(() {
         _currentP = LatLng(currentLocation.latitude!, currentLocation.longitude!);
         _cameraToPosition(_currentP!);
+        sendLocationUpdate(_currentP!);
       });
     });
   }
-Future<void> _cameraToPosition(LatLng pos) async {
+
+  Future<void> _cameraToPosition(LatLng pos) async {
     final GoogleMapController controller = await _mapController.future;
     CameraPosition _newCameraPosition = CameraPosition(
       target: pos,
@@ -78,14 +83,15 @@ Future<void> _cameraToPosition(LatLng pos) async {
       CameraUpdate.newCameraPosition(_newCameraPosition),
     );
   }
+
   Future<List<LatLng>> getPolylinePoints() async {
     List<LatLng> polylineCoordinates = [];
     PolylinePoints polylinePoints = PolylinePoints();
     PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
       'AIzaSyBkAzB6UX78XjJ1RWx30Qwn3Mvf9Vdd64Q',
       PointLatLng(_currentP!.latitude, _currentP!.longitude),
-          PointLatLng(_ambulanceLocation.latitude, _ambulanceLocation.longitude),
-          travelMode: TravelMode.driving,
+      PointLatLng(_ambulanceLocation.latitude, _ambulanceLocation.longitude),
+      travelMode: TravelMode.driving,
     );
     if (result.points.isNotEmpty) {
       for (var point in result.points) {
@@ -96,22 +102,54 @@ Future<void> _cameraToPosition(LatLng pos) async {
     }
     return polylineCoordinates;
   }
-  
+
   void generatePolyLineFromPoints(List<LatLng> polylineCoordinates) async {
     PolylineId id = PolylineId("poly");
     Polyline polyline = Polyline(
-        polylineId: id,
-        color: Colors.black,
-        points: polylineCoordinates,
-        width: 8);
+      polylineId: id,
+      color: Colors.black,
+      points: polylineCoordinates,
+      width: 8,
+    );
     setState(() {
       polylines[id] = polyline;
     });
-    
-  // Future<void> _searchLocation(String query) async {
-         
-  // }
   }
+
+  void initSocket() {
+    socket = IO.io('http://your-node-server.com', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+
+    socket.connect();
+
+    socket.onConnect((data) {
+      print('Connected to server');
+    });
+
+    socket.on('update-ambulance-location', (data) {
+      updateAmbulanceLocation(data);
+    });
+  }
+
+  void sendLocationUpdate(LatLng location) {
+    socket.emit('update-location', {
+      'latitude': location.latitude,
+      'longitude': location.longitude,
+    });
+  }
+
+  void updateAmbulanceLocation(dynamic data) {
+    LatLng ambulanceLocation = LatLng(data['latitude'], data['longitude']);
+    setState(() {
+      _ambulanceLocation = ambulanceLocation;
+      getPolylinePoints().then((coordinates) => {
+            generatePolyLineFromPoints(coordinates),
+          });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -174,9 +212,8 @@ Future<void> _cameraToPosition(LatLng pos) async {
                 Marker(
                   markerId: MarkerId('ambulance'),
                   icon: BitmapDescriptor.defaultMarker,
-                  position: _nyabihuLocation,
-                  infoWindow: InfoWindow(
-                    title: 'Ambulance Location',
+                  position: _ambulanceLocation,
+                  infoWindow: InfoWindow(title: 'Ambulance Location',
                     snippet: 'Kigali, Rwanda',
                   ),
                 ),
@@ -190,7 +227,7 @@ Future<void> _cameraToPosition(LatLng pos) async {
                   ),
                 ),
               },
-             polylines: Set<Polyline>.of(polylines.values),
+              polylines: Set<Polyline>.of(polylines.values),
             ),
           ),
           Padding(
